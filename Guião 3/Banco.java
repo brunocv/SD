4a943362bde7caada5b5053c643534ddfb5d6203
brunocv <brunocveloso@sapo.pt>
2019-10-16 12:02:47 +0100

    mais guiao 3

--- Guião 3/Banco.java
+++ Guião 3/Banco.java
@@ -11,12 +11,42 @@ public class Banco {
 
     public int createAccount(float x){
         
+        int i=contas.size();
+        
+        while(contas.containsKey(i)) i++;
+
         Conta nova = new Conta(x);
-        contas.put(contas.size(),nova);
-        return (contas.size() - 1);
+        contas.put(i,nova);
+        return (i);
     }
     
     public float closeAccount(int id) throws InvalidAccount{
+        
+        if(this.contas.containsKey(id)) {
+            float saldo = this.contas.get(id).consulta();
+            this.contas.remove(id);
+            return saldo;
+        }
+        
         return 0;
     }
+
+
+    public float totalBalance(int accounts[]){
+        
+        float total=0;
+        
+        for(int x: accounts){
+            if(contas.containsKey(x))
+                total += contas.get(x).consulta();
+        }
+        
+        return total;
+
+    }
+
+    public void transfer(int from, int to, float amount) throws InvalidAccount, NotEnoughFunds{
+    
+    }
+
 }
--- Guião 3/Cliente1.java
+++ Guião 3/Cliente1.java
@@ -10,6 +10,16 @@ public class Cliente1 implements Runnable{
         for(int i=0; i<5; i++){
             System.out.println(cr.createAccount(10*(i+1)));
         }
+    
+        int[] array = new int[]{0,1,2,3,4}; 
+        System.out.println("Saldo total: "+ cr.totalBalance(array));
+        
+        try{
+            System.out.println("Apaguei: " + cr.closeAccount(4));
+        }
+        catch(InvalidAccount e){
+        }
+
     }
 
 }
